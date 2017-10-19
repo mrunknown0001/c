a315db38a738c21d4090b2874728db790359663b
Michael Adam <madamt0001@gmail.com>
2017-10-20 06:19:40 +0800

    added 503 error

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,50 @@
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Serve Maintenace</title>
+
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{  asset('css/full-slider.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+    <link rel="icon" href="{{ asset('uploads/images/logo.ico') }}">
+
+    <style type="text/css">
+        body {
+            background: url("{{ url('uploads/images/errors/') }}") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            -o-background-size: cover;
+        }
+    </style>
+
+  </head>
+
+  <body>
+  <div class="wrapper">
+    <br>
+    <h1 class="text-center">
+        Server Maintenace. Be Right Back
+    </h1>
+    <p class="text-center">
+        <img style="height: auto; width: 100%;" src="{{ url('uploads/images/make-icon.jpg') }}" class="img-responsive" alt="CLLR Logo">
+    </p>
+  </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/popper/popper.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+
+  </body>
+
+</html>
