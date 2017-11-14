2cc5c1c88b383cf1a6865c7f660f1a9928d624e8
Michael Adam <madamt0001@gmail.com>
2017-11-15 05:58:41 +0800

    update

--- resources/views/modal-register.blade.php
+++ resources/views/modal-register.blade.php
@@ -4,16 +4,18 @@
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header modal-header-success">
-                <div class="pull-left">Registration Terms and Conditions</div>
+                <div class="pull-left">CLLR Trading Terms and Conditions</div>
                 <div class="pull-right"><button type="button" class="close" data-dismiss="modal">&times;</button></div>
             </div>
             <div class="modal-body modal-body-register">
-                <p>Condition Here</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet dicta earum minus quo nihil laudantium nam dolorem, accusamus quia libero odio voluptas quis, aliquid reiciendis magnam! Debitis, odit vitae.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, laborum saepe non, debitis veniam aspernatur, a natus repellat esse minus aperiam dignissimos error ut ex incidunt veritatis, fugiat quos quas.</p>
+                <ul>
+                    <li>By Registering in this web site, you agree to the terms and condions of CLLR Trading</li>
+                </ul>
+                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet dicta earum minus quo nihil laudantium nam dolorem, accusamus quia libero odio voluptas quis, aliquid reiciendis magnam! Debitis, odit vitae.</p> -->
+                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, laborum saepe non, debitis veniam aspernatur, a natus repellat esse minus aperiam dignissimos error ut ex incidunt veritatis, fugiat quos quas.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ab quam minima fugit illo, odio dolor, id culpa soluta accusantium! Libero sed nesciunt id, magnam quod in praesentium, excepturi sunt.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere odit voluptates nemo saepe voluptate cumque cum rem enim repellendus adipisci, hic quod. In vero nostrum sed repellat, sequi labore eveniet.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet architecto quas, est tenetur doloribus libero? Consequuntur, esse aut quos repudiandae dolore quia fuga hic quasi, nulla amet, optio dolor facilis.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet architecto quas, est tenetur doloribus libero? Consequuntur, esse aut quos repudiandae dolore quia fuga hic quasi, nulla amet, optio dolor facilis.</p> -->
             </div>
             <div class="modal-footer">
                 <a href="javascript: void();" class="btn btn-danger" data-dismiss="modal">I disagree</a>
