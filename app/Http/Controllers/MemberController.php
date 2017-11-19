99140062f4f7567d59e0c72d1326c1215f40042c
Michael Adam <madamt0001@gmail.com>
2017-11-19 16:34:53 +0800

    update

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -261,7 +261,7 @@ class MemberController extends Controller
 
         // send email first before saving
         // send email confirmation, to active status, containing the code
-        // Mail::to($email)->send(new ConfirmRegistration($code));
+        Mail::to($email)->send(new ConfirmRegistration($code));
         // send sms to the user if requested
 
 
@@ -449,7 +449,7 @@ class MemberController extends Controller
 
 
     		// send welcome email and/or sms
-            // Mail::to($user->email)->send(new WelcomeEmail($user));
+            Mail::to($user->email)->send(new WelcomeEmail($user));
             // email is temporaryly inactive
             // sendSms
             $message = 'Hi ' . ucwords($user->firstname) . '!
@@ -533,7 +533,7 @@ CLLR Trading Team';
       $reset->save();
 
       // send reset link to email of the user
-      // Mail::to($email)->send(new PasswordResetLink($token));
+      Mail::to($email)->send(new PasswordResetLink($token));
       
 
       // userlog
